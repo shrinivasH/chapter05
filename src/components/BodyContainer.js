b9f1c06ca88714834568e78eed509cdaf6318d10
User <user@Users-MacBook-Pro-136.local>
2023-10-05 17:44:00 +0530

    API integration is done.

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,20 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
+import { FilterIcon } from "../icons/FilterIcon";
+import { SWIGGY_APP_URL } from "../utils/constants";
+import Shimmer from "./Shimmer";
 
 const BodyContainer = () => {
-  return (
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    getRestaurant();
+  }, []);
+  const getRestaurant = async () => {
+    const response = await fetch(SWIGGY_APP_URL);
+    const json = await response.json();
+    setListOfRestaurant(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search a restaurant..."
-          className="search-input"
-        />
-        <button className="search-btn">Search</button>
+      <div className="meta-info">
+        <div className="search-parent">
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Search a restaurant..."
+              className="search-input"
+              value={searchText}
+              onChange={(e) => {
+                setSearchText(e.target.value);
+              }}
+            />
+            <button
+              className="search-btn"
+              onClick={() => {
+                const filteredList = listOfRestaurant.filter((res) => {
+                  return res.info.name
+                    .toLocaleLowerCase()
+                    .includes(searchText.toLocaleLowerCase());
+                });
+                setFilteredRestaurant(filteredList);
+              }}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let result = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurant(result);
+            console.log("Result", result);
+          }}
+        >
+          <span className="btn-sp"> Filter</span> <FilterIcon></FilterIcon>
+        </button>
       </div>
+
       <div className="res-container">
-        {restaurantList.map((restaurnat) => (
+        {filteredRestaurant.map((restaurnat) => (
           <RestaurantCard key={restaurnat.info.id} resData={restaurnat} />
         ))}
       </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,6 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvSnrKDj1mVXJ9kyyj-ecCnJBIPl8uhMmbQ_8dohxY&s";
-
+export const SWIGGY_APP_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.6180608&lng=73.77058459999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 /* Named export */
